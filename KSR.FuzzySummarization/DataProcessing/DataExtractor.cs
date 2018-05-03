34032e310c09ad6d007b4f2c6c1348f0e436082e
Piotr Szymczak <dogier140@poczta.fm>
2018-05-03 15:47:07 +0200

    Data parsing

---
+++ KSR.FuzzySummarization/DataProcessing/DataExtractor.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using KSR.FuzzySummarization.Model;
+
+namespace KSR.FuzzySummarization.DataProcessing
+{
+    public class DataExtractor
+    {
+
+        public IEnumerable<DataRecord> ObtainRecords()
+        {
+            var lines = File.ReadAllLines("Data/data.txt");
+            foreach (var line in lines.Where(s => !s.Contains("?")))
+            {
+                var tokens = line.Split(new [] {','} ,StringSplitOptions.RemoveEmptyEntries).Select(s => s.Trim()).ToList();
+                if(!tokens.Any())
+                    continue;
+                yield return new DataRecord
+                {
+                    Age = int.Parse(tokens[0]),
+                    Workclass = (Workclass) WorkClasses.IndexOf(tokens[1]),
+                    SamplingWeight = int.Parse(tokens[2]),
+                    Education = (Education) Educations.IndexOf(tokens[3]),
+                    EductaionNumber = int.Parse(tokens[4]),
+                    MaritalStatus = (MaritalStatus) MaritalStatuses.IndexOf(tokens[5]),
+                    Occupation = (Occupation) Occupations.IndexOf(tokens[6]),
+                    Relationship = (Relationship) Relationships.IndexOf(tokens[7]),
+                    Race = (Race) Races.IndexOf(tokens[8]),
+                    IsMale = tokens[9] == "Male",
+                    CapitalGain = int.Parse(tokens[10]),
+                    CapitalLoss = int.Parse(tokens[11]),
+                    HoursPerWeek = int.Parse(tokens[12]),
+                    NativeCountry = (NativeCountry) NativeCountries.IndexOf(tokens[13])
+                };
+            }
+        }
+
+
+        private List<string> WorkClasses = new List<string> { "Private", "Self-emp-not-inc", "Self-emp-inc", "Federal-gov", "Local-gov", "State-gov", "Without-pay", "Never-worked" };
+        private List<string> Educations = new List<string> { "Bachelors", "Some-college", "11th", "HS-grad", "Prof-school", "Assoc-acdm", "Assoc-voc", "9th", "7th-8th", "12th", "Masters", "1st-4th", "10th", "Doctorate", "5th-6th", "Preschool" };
+        private List<string> MaritalStatuses = new List<string> { "Married-civ-spouse", "Divorced", "Never-married", "Separated", "Widowed", "Married-spouse-absent", "Married-AF-spouse" };
+        private List<string> Occupations = new List<string> { "Tech-support", "Craft-repair", "Other-service", "Sales", "Exec-managerial", "Prof-specialty", "Handlers-cleaners", "Machine-op-inspct", "Adm-clerical", "Farming-fishing", "Transport-moving", "Priv-house-serv", "Protective-serv", "Armed-Forces" };
+        private List<string> Relationships = new List<string> { "Wife", "Own-child", "Husband", "Not-in-family", "Other-relative", "Unmarried" };
+        private List<string> Races = new List<string> { "White", "Asian-Pac-Islander", "Amer-Indian-Eskimo", "Other", "Black" };
+        private List<string> NativeCountries = new List<string> { "United-States", "Cambodia", "England", "Puerto-Rico", "Canada", "Germany", "Outlying-US(Guam-USVI-etc)", "India", "Japan", "Greece", "South", "China", "Cuba", "Iran", "Honduras", "Philippines", "Italy", "Poland", "Jamaica", "Vietnam", "Mexico", "Portugal", "Ireland", "France", "Dominican-Republic", "Laos", "Ecuador", "Taiwan", "Haiti", "Columbia", "Hungary", "Guatemala", "Nicaragua", "Scotland", "Thailand", "Yugoslavia", "El-Salvador", "Trinadad&Tobago", "Peru", "Hong", "Holand-Netherlands" };
+
+    }
+}
---
+++ KSR.FuzzySummarization/Model/DataRecord.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KSR.FuzzySummarization.Model
+{
+    public class DataRecord
+    {
+        public int Age { get; set; }
+        public Workclass Workclass { get; set; }
+        public int SamplingWeight { get; set; }
+        public Education Education { get; set; }
+        public int EductaionNumber { get; set; }
+        public MaritalStatus MaritalStatus { get; set; }
+        public Occupation Occupation { get; set; }
+        public Relationship Relationship { get; set; }
+        public Race Race { get; set; }
+        public bool IsMale { get; set; }
+        public int CapitalGain { get; set; }
+        public int CapitalLoss { get; set; }
+        public int HoursPerWeek { get; set; }
+        public NativeCountry NativeCountry { get; set; }
+    }
+
+    public enum Workclass
+    {
+        Private,
+        SelpEmployedNotIncorporated,
+        SelfEmployedIncorporated,
+        FederalGovernor,
+        LocalGovernor,
+        StateGovernor,
+        WithoutPay,
+        NeverWorked
+    }
+
+    public enum Education
+    {
+        Bachelor,
+        College,
+        Grade11,
+        HsGraduate,
+        ProfSchool,
+        AcademicAssociative,
+        VocAssociative,
+        Grade9,
+        Grade7Or8,
+        Grade12,
+        Masters,
+        Grade1To4,
+        Grade10,
+        Doctorate,
+        Grade5To6,
+        Preschool
+    }
+
+    public enum MaritalStatus
+    {
+        MarriedCivSpouse,
+        Divorced,
+        NeverMarried,
+        Separated,
+        Widowed,
+        MarriedSpouseAbsent,
+        MarriedAfSpouse
+    }
+
+    public enum Occupation
+    {
+        TechSupport,
+        CraftRepair,
+        OtherService,
+        Sales,
+        ExecManagerial,
+        ProfSpecialty,
+        HandlersCleaners,
+        MachineOpInspct,
+        AdmClerical,
+        FarmingFishing,
+        TransportMoving,
+        PrivHouseServ,
+        ProtectiveServ,
+        ArmedForces
+    }
+
+    public enum Relationship
+    {
+        Wife,
+        OwnChild,
+        Husband,
+        NotInFamily,
+        OtherRelative,
+        Unmarried
+    }
+
+    public enum Race
+    {
+        White,
+        AsianPacIslander,
+        AmerIndianEskimo,
+        Other,
+        Black
+    }
+
+    public enum NativeCountry
+    {
+        UnitedSStates,
+        Cambodia,
+        England,
+        PuertoRico,
+        Canada,
+        Germany,
+        OutlyingUs,
+        India,
+        Japan,
+        Greece,
+        South,
+        China,
+        Cuba,
+        Iran,
+        Honduras,
+        Philippines,
+        Italy,
+        Poland,
+        Jamaica,
+        Vietnam,
+        Mexico,
+        Portugal,
+        Ireland,
+        France,
+        DominicanRepublic,
+        Laos,
+        Ecuador,
+        Taiwan,
+        Haiti,
+        Columbia,
+        Hungary,
+        Guatemala,
+        Nicaragua,
+        Scotland,
+        Thailand,
+        Yugoslavia,
+        ElSalvador,
+        TrinadadTobago,
+        Peru,
+        Hong,
+        HolandNetherlands
+    }
+}
